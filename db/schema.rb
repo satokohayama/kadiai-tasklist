c6167c0125cf6e85c97f40240e4aed20f9777322
jiro.kirameki <a13124hs@gmail.com>
2017-04-24 06:05:59 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170424041005) do
+ActiveRecord::Schema.define(version: 20170424055934) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
